2dfe34e18cf1ca9c86a67c6c4f434a8d39621163
LanikTop <russaf05@gmail.com>
2024-01-08 20:26:00 +0500

    Version 1.9 1. Изменение игры - Появление анимации движения корабля - Рестарт игры и выход после смерти

--- SpaceShip.py
+++ SpaceShip.py
@@ -28,7 +28,10 @@ class SpaceShip:
         self.all_sprites = pygame.sprite.Group()
         self.ship = pygame.sprite.Sprite()
         self.ship.image = load_image('ship.png', -1)
+        self.ship.image_top = pygame.transform.scale(self.ship.image, (150, 150))
         self.ship.image = pygame.transform.scale(self.ship.image, (150, 150))
+        self.ship.image_left = pygame.transform.rotate(self.ship.image, -45)
+        self.ship.image_right = pygame.transform.rotate(self.ship.image, 45)
         self.ship.rect = self.ship.image.get_rect()
         self.all_sprites.add(self.ship)
         self.ship.rect.x = size1 // 2 - 75
@@ -78,9 +81,9 @@ def start_game_buttle(player=1):
     cur = con.cursor()
     result = cur.execute(f"""SELECT * FROM info_users WHERE id = {player}""").fetchone()
     money = result[1]
-    player_speed = 10 + 5 * result[2]
-    bullet_rate = 225 - 25 * result[3]
-    bullet_speed = 6 + 1 * result[4]
+    player_speed = 10 + 10 * result[2]
+    bullet_rate = 225 - 50 * result[3]
+    bullet_speed = 6 + 3 * result[4]
     highscore = result[5]
     meteor_speed = 1
     # Создание окна
@@ -89,7 +92,8 @@ def start_game_buttle(player=1):
     # Загрузка музыки
     pygame.mixer.music.load('sounds/space_sound.mp3')
     game_over_sound = pygame.mixer.Sound('sounds/game_over.ogg')
-    pygame.mixer.music.set_volume(0.015)
+    # pygame.mixer.music.load('sounds/game_over.mp3')
+    pygame.mixer.music.set_volume(0.1)
     pygame.mixer.music.play(-1)
     # Задний фон
     backround = load_image('backround.png', -1)
@@ -130,7 +134,7 @@ def start_game_buttle(player=1):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_down = True
             if event.type == pygame.MOUSEBUTTONUP:
-                if event.pos[0] >= (screen.get_width() - 150) and event.pos[1] <= 150:
+                if life is True and event.pos[0] >= (screen.get_width() - 150) and event.pos[1] <= 150:
                     life = 'pause'
                 if life == 'pause' and screen.get_width() // 3 < event.pos[0] < (screen.get_width() * 2 // 3) and int(
                         screen.get_height() // 3.5) < event.pos[1] < int(screen.get_height() // 3.5) + 150:
@@ -139,6 +143,12 @@ def start_game_buttle(player=1):
                         screen.get_width() * 2 // 3) and screen.get_height() // 2 < event.pos[1] < (
                         screen.get_height() // 2 + 150):
                     life = 'exit'
+                if not life and screen.get_width() // 2 < event.pos[0] < (screen.get_width() * 5 // 6) \
+                        and int(screen.get_height() * 0.8) < event.pos[1] < int(screen.get_height() * 0.8) + 150:
+                    life = 'exit'
+                if not life and screen.get_width() // 10 < event.pos[0] < (screen.get_width() * 13 // 30) \
+                        and int(screen.get_height() * 0.8) < event.pos[1] < int(screen.get_height() * 0.8) + 150:
+                    life = 'restart'
             if event.type == pygame.MOUSEMOTION and mouse_down:
                 x, y = event.pos[0] - 75, event.pos[1] - 75
                 if not go_move:
@@ -147,11 +157,11 @@ def start_game_buttle(player=1):
                 mouse_down = False
         # Проверка смерти
         for elem in meteorits:
-            if pygame.sprite.collide_mask(ship.ship, elem):
+            if pygame.sprite.collide_mask(ship.ship, elem) and life is True:
                 life = False
         # Режим паузы
         if life == 'pause':
-            pygame.mixer.music.pause() # -> Поставить на паузу
+            pygame.mixer.music.pause()  # -> Поставить на паузу
             pygame.draw.rect(screen, pygame.Color((128, 128, 128)), (
                 screen.get_width() // 4, screen.get_height() // 4, screen.get_width() // 2, screen.get_height() // 2))
             screen.blit(continuue, (screen.get_width() // 3, int(screen.get_height() // 3.5)))
@@ -160,20 +170,27 @@ def start_game_buttle(player=1):
         elif life == 'exit':
             running = False
             cur.execute(f"""UPDATE info_users SET money = '{money}' WHERE id = '{player}'""")
+        elif life == 'restart':
+            running = False
+            start_game_buttle()
         # Режим игры
         elif life:
-            pygame.mixer.music.unpause() # -> Снять паузу
+            pygame.mixer.music.unpause()  # -> Снять паузу
             if go_move and ship.ship.rect.x == x and ship.ship.rect.y == y:
                 go_move = False
             # Движение корабля
             if go_move:
                 if ship.ship.rect.x < x:
+                    ship.ship.image = ship.ship.image_left
                     ship.move_ship(player_speed, 0)
                     if ship.ship.rect.x > x:
+                        ship.ship.image = ship.ship.image_top
                         ship.ship.rect.x = x
                 if ship.ship.rect.x > x:
+                    ship.ship.image = ship.ship.image_right
                     ship.move_ship(-player_speed, 0)
                     if ship.ship.rect.x < x:
+                        ship.ship.image = ship.ship.image_top
                         ship.ship.rect.x = x
                 if ship.ship.rect.y < y:
                     ship.move_ship(0, player_speed)
@@ -244,14 +261,16 @@ def start_game_buttle(player=1):
             # gameover
             pygame.mixer.music.stop()
             game_over_sound.set_volume(0.1)
-            # game_over_sound.play(1) # -> Звук game over
+            game_over_sound.play(1)  # -> Звук game over
 
             screen.fill((0, 0, 0))
             gameover_image = load_image('gameover.png', -1)
-            gameover_image = pygame.transform.scale(gameover_image, screen.get_size())
+            gameover_image = pygame.transform.scale(gameover_image, (screen.get_width(), screen.get_height() - 100))
             screen.blit(gameover_image, (0, 0))
             restart_image = load_image('restart.png', -1)
-            screen.blit(restart_image, (screen.get_width() // 4, screen.get_height() // 4 * 3))
+            restart_image = pygame.transform.scale(restart_image, (screen.get_width() // 3, 150))
+            screen.blit(restart_image, (screen.get_width() // 10, int(screen.get_height() * 0.8)))
+            screen.blit(quit, (screen.get_width() // 2, int(screen.get_height() * 0.8)))
         pygame.display.flip()
     game_over_sound.stop()
     con.commit()
