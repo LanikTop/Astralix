a0b3fd5c7a591fe134ae291d0368335fb87aaf71
LanikTop <russaf05@gmail.com>
2024-01-11 00:27:55 +0500

    Version 1.9.2 1. Технической обновление - Файл requirements.txt - Фикс багов с магазином

--- main.py
+++ main.py
@@ -49,6 +49,15 @@ class Rules_Window(QDialog, Ui_Rules_Window):
         self.close()
 
 
+PRICE_PLAYER_SPEED = 2
+PRICE_SHOOT_RATE = 2
+PRICE_SHOOT_SPEED = 2
+
+COUNT_PS = 1
+COUNT_SR = 1
+COUNT_SS = 1
+
+
 class Shop_Window(QDialog, Ui_Shop_Window):
     def __init__(self):
         super().__init__()
@@ -62,161 +71,87 @@ class Shop_Window(QDialog, Ui_Shop_Window):
         self.shoot_rate_value = self.cur.execute('''SELECT shoot_rate FROM info_users WHERE id=1''').fetchone()[0]
         self.shoot_speed_value = self.cur.execute('''SELECT shoot_speed FROM info_users WHERE id=1''').fetchone()[0]
 
-        # player speed
-        if self.player_speed_value == 2:
-            self.player_speed_level_1.setEnabled(False)
-        if 3 <= self.player_speed_value >= 2:
-            self.player_speed_level_1.setEnabled(False)
-            self.player_speed_level_2.setEnabled(False)
-        if 4 <= self.player_speed_value >= 2:
-            self.player_speed_level_1.setEnabled(False)
-            self.player_speed_level_2.setEnabled(False)
-            self.player_speed_level_3.setEnabled(False)
-        # shoot rate
-        if self.shoot_rate_value == 2:
-            self.shoot_rate_level_1.setEnabled(False)
-        if 3 <= self.shoot_rate_value >= 2:
-            self.shoot_rate_level_1.setEnabled(False)
-            self.shoot_rate_level_2.setEnabled(False)
-        if 4 <= self.shoot_rate_value >= 2:
-            self.shoot_rate_level_1.setEnabled(False)
-            self.shoot_rate_level_2.setEnabled(False)
-            self.shoot_rate_level_3.setEnabled(False)
-        # shoot speed
-        if self.shoot_speed_value == 2:
-            self.shoot_speed_level_1.setEnabled(False)
-        if 3 <= self.shoot_speed_value >= 2:
-            self.shoot_speed_level_1.setEnabled(False)
-            self.shoot_speed_level_2.setEnabled(False)
-        if 4 <= self.shoot_speed_value >= 2:
-            self.shoot_speed_level_1.setEnabled(False)
-            self.shoot_speed_level_2.setEnabled(False)
-            self.shoot_speed_level_3.setEnabled(False)
+        self.level_1.setText(f'Уровень {self.player_speed_value}')
+        self.level_2.setText(f'Уровень {self.shoot_rate_value}')
+        self.level_3.setText(f"Уровень {self.shoot_speed_value}")
 
+        self.player_speed_level_1.setText(f'{2 ** self.player_speed_value} pl')
+        self.shoot_rate_level_1.setText(f'{2 ** self.shoot_rate_value} sr')
+        self.shoot_speed_level_1.setText(f'{2 ** self.shoot_speed_value} ss')
 
         self.go_back_button.clicked.connect(self.go_back)
+
         self.player_speed_level_1.clicked.connect(self.buy_boost)
         self.player_speed_level_1.setIcon(QtGui.QIcon('data\money.png'))
         self.player_speed_level_1.setIconSize(QtCore.QSize(40, 40))
-
-        self.player_speed_level_2.clicked.connect(self.buy_boost)
-        self.player_speed_level_2.setIcon(QtGui.QIcon('data\money.png'))
-        self.player_speed_level_2.setIconSize(QtCore.QSize(40, 40))
-
-        self.player_speed_level_3.clicked.connect(self.buy_boost)
-        self.player_speed_level_3.setIcon(QtGui.QIcon('data\money.png'))
-        self.player_speed_level_3.setIconSize(QtCore.QSize(40, 40))
 
         self.shoot_rate_level_1.clicked.connect(self.buy_boost)
         self.shoot_rate_level_1.setIcon(QtGui.QIcon('data\money.png'))
         self.shoot_rate_level_1.setIconSize(QtCore.QSize(40, 40))
 
-        self.shoot_rate_level_2.clicked.connect(self.buy_boost)
-        self.shoot_rate_level_2.setIcon(QtGui.QIcon('data\money.png'))
-        self.shoot_rate_level_2.setIconSize(QtCore.QSize(40, 40))
-
-        self.shoot_rate_level_3.clicked.connect(self.buy_boost)
-        self.shoot_rate_level_3.setIcon(QtGui.QIcon('data\money.png'))
-        self.shoot_rate_level_3.setIconSize(QtCore.QSize(40, 40))
-
         self.shoot_speed_level_1.clicked.connect(self.buy_boost)
         self.shoot_speed_level_1.setIcon(QtGui.QIcon('data\money.png'))
         self.shoot_speed_level_1.setIconSize(QtCore.QSize(40, 40))
-
-        self.shoot_speed_level_2.clicked.connect(self.buy_boost)
-        self.shoot_speed_level_2.setIcon(QtGui.QIcon('data\money.png'))
-        self.shoot_speed_level_2.setIconSize(QtCore.QSize(40, 40))
-
-        self.shoot_speed_level_3.clicked.connect(self.buy_boost)
-        self.shoot_speed_level_3.setIcon(QtGui.QIcon('data\money.png'))
-        self.shoot_speed_level_3.setIconSize(QtCore.QSize(40, 40))
 
         self.check_balance.setText(str(self.balance))
         self.no_money_label.hide()
 
     def buy_boost(self):
+        global PRICE_SHOOT_RATE
+        global PRICE_SHOOT_SPEED
+        global PRICE_PLAYER_SPEED
+
+        global COUNT_PS
+        global COUNT_SR
+        global COUNT_SS
+
         button = QApplication.instance().sender()
         text = button.text().split()
+
         if self.balance >= int(text[0]):
-            #player speed ↓ ↓ ↓ ↓
-            if text[0] == '5' and text[1] == 'pl':
-                self.cur.execute('''UPDATE info_users SET player_speed=2 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 5
+            # player speed ↓ ↓ ↓ ↓
+            if text[1] == 'pl':
+                self.cur.execute('''UPDATE info_users SET player_speed=player_speed+1 WHERE id=1''')
+                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (PRICE_PLAYER_SPEED,))
+                self.balance -= PRICE_PLAYER_SPEED
                 self.check_balance.setText(str(self.balance))
-                self.player_speed_level_1.setEnabled(False)
-                self.con.commit()
-            elif text[0] == '15' and text[1] == 'pl':
-                self.cur.execute('''UPDATE info_users SET player_speed=3 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 15
-                self.check_balance.setText(str(self.balance))
-                self.player_speed_level_2.setEnabled(False)
-                self.con.commit()
-            elif text[0] == '30' and text[1] == 'pl':
-                self.cur.execute('''UPDATE info_users SET player_speed=4 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 30
-                self.check_balance.setText(str(self.balance))
-                self.player_speed_level_3.setEnabled(False)
+                COUNT_PS += 1
+                PRICE_PLAYER_SPEED = 2 ** COUNT_PS
+                self.player_speed_level_1.setText(f'{PRICE_PLAYER_SPEED} pl')
+                self.level_1.setText(f"Уровень {COUNT_PS}")
                 self.con.commit()
             # shoot rate ↓ ↓ ↓ ↓
-            elif text[0] == '5' and text[1] == 'sr':
-                self.cur.execute('''UPDATE info_users SET shoot_rate=2 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 5
+            elif text[1] == 'sr':
+                self.cur.execute('''UPDATE info_users SET shoot_rate=shoot_rate+1 WHERE id=1''')
+                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (PRICE_SHOOT_RATE,))
+                self.balance -= PRICE_SHOOT_RATE
                 self.check_balance.setText(str(self.balance))
-                self.shoot_rate_level_1.setEnabled(False)
-                self.con.commit()
-            elif text[0] == '15' and text[1] == 'sr':
-                self.cur.execute('''UPDATE info_users SET shoot_rate=3 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 15
-                self.check_balance.setText(str(self.balance))
-                self.shoot_rate_level_2.setEnabled(False)
-                self.con.commit()
-            elif text[0] == '30' and text[1] == 'sr':
-                self.cur.execute('''UPDATE info_users SET shoot_rate=4 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 30
-                self.check_balance.setText(str(self.balance))
-                self.shoot_rate_level_3.setEnabled(False)
+                COUNT_SR += 1
+                PRICE_SHOOT_RATE = 2 ** COUNT_SR
+                self.shoot_rate_level_1.setText(f'{PRICE_SHOOT_RATE} sr')
+                self.level_2.setText(f"Уровень {COUNT_SR}")
                 self.con.commit()
             # shoot_speed ↓ ↓ ↓ ↓
-            elif text[0] == '5' and text[1] == 'ss':
-                self.cur.execute('''UPDATE info_users SET shoot_speed=2 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 5
+            elif text[1] == 'ss':
+                self.cur.execute('''UPDATE info_users SET shoot_speed=shoot_speed+1 WHERE id=1''')
+                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (PRICE_SHOOT_SPEED,))
+                self.balance -= PRICE_SHOOT_SPEED
                 self.check_balance.setText(str(self.balance))
-                self.shoot_speed_level_1.setEnabled(False)
+                COUNT_SS += 1
+                PRICE_SHOOT_SPEED = 2 ** COUNT_SS
+                self.shoot_speed_level_1.setText(f'{PRICE_SHOOT_SPEED} ss')
+                self.level_3.setText(f"Уровень {COUNT_SS}")
                 self.con.commit()
-            elif text[0] == '15' and text[1] == 'ss':
-                self.cur.execute('''UPDATE info_users SET shoot_speed=3 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 15
-                self.check_balance.setText(str(self.balance))
-                self.shoot_speed_level_2.setEnabled(False)
-                self.con.commit()
-            elif text[0] == '30' and text[1] == 'ss':
-                self.cur.execute('''UPDATE info_users SET shoot_speed=4 WHERE id=1''')
-                self.cur.execute('''UPDATE info_users SET money=money - ? WHERE id=1''', (int(text[0]),))
-                self.balance -= 30
-                self.check_balance.setText(str(self.balance))
-                self.shoot_speed_level_3.setEnabled(False)
-                self.con.commit()
-
         else:
             self.print_no_money()
 
     def print_no_money(self):
         self.no_money_label.show()
+
     def go_back(self):
         self.start_ex = Start_Window()
         self.start_ex.show()
         self.close()
-
-
-
 
 
 if __name__ == '__main__':
